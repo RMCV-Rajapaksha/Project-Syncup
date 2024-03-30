cefe640c54dc4caf2b2c446d53a4b5cc2c200f44
Viranga Weerabandara <umiduviranga8@gmail.com>
2024-03-30 15:23:15 +0530

    Refactor theme and login pages

--- lib/commponnent/theme.dart
+++ lib/commponnent/theme.dart
@@ -1,79 +1,88 @@
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Template extends StatelessWidget {
-  const Template(
-      {super.key,
-      required this.screenWidth,
-      required this.screenHeight,
-      required this.theChild});
+  Template({
+    super.key,
+    required this.screenWidth,
+    required this.screenHeight,
+    required this.theChild,
+  });
   final screenWidth;
   final screenHeight;
   final theChild;
+
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      child: Container(
-          width: screenWidth,
-          height: screenHeight,
-          color: Colors.white,
-          child: Stack(
-            children: [
-              Opacity(
-                opacity: 0.85,
-                child: Container(
-                  width: screenWidth,
-                  height: screenHeight,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Color.fromRGBO(57, 189, 176, 0.8),
-                          Color.fromRGBO(35, 92, 163, 0.8),
-                        ]),
-                  ),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: screenHeight * 0.125,
-                        width: screenWidth,
+    return MaterialApp(
+      home: Scaffold(
+        body: ClipRRect(
+          child: Container(
+              width: screenWidth,
+              height: screenHeight,
+              color: Colors.white,
+              child: Stack(
+                children: [
+                  Opacity(
+                    opacity: 0.85,
+                    child: Container(
+                      width: screenWidth,
+                      height: screenHeight,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                              Color.fromRGBO(57, 189, 176, 0.8),
+                              Color.fromRGBO(35, 92, 163, 0.8),
+                            ]),
                       ),
-                      Container(
-                        width: screenWidth,
-                        height: screenHeight * 0.6,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/images/Background.png'),
-                            fit: BoxFit
-                                .cover, // Ensure the image covers the entire container
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: screenHeight * 0.125,
+                            width: screenWidth,
                           ),
-                        ),
+                          Container(
+                            width: screenWidth,
+                            height: screenHeight * 0.6,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/Background.png'),
+                                fit: BoxFit
+                                    .cover, // Ensure the image covers the entire container
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: screenHeight * 0.275,
+                            width: screenWidth,
+                          )
+                        ],
                       ),
-                      SizedBox(
-                        height: screenHeight * 0.275,
-                        width: screenWidth,
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
-                  child: Container()),
-              Container(
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                      Colors.white.withOpacity(0.15),
-                      Colors.white.withOpacity(0.05)
-                    ])),
-              ),
-              Center(child: theChild)
-            ],
-          )),
+                  BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                      child: Container()),
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            colors: [
+                          Colors.white.withOpacity(0.15),
+                          Colors.white.withOpacity(0.05)
+                        ])),
+                  ),
+                  Center(child: theChild),
+                ],
+              )),
+        ),
+      ),
     );
   }
 }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -7,21 +7,23 @@ class Login extends StatefulWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _ProductState();
+  State<Login> createState() => _LoginState();
 }
 
-class _ProductState extends State<Login> {
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return Template(
-        screenWidth: screenWidth,
-        screenHeight: screenHeight,
-        theChild: const Text('SyncUp Login Page',
-            style: TextStyle(
-                color: Color(0xE4303643),
-                fontSize: 30,
-                fontWeight: FontWeight.bold)));
+      screenWidth: screenWidth,
+      screenHeight: screenHeight,
+      theChild: TextField(
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Enter your username',
+        ),
+      ),
+    );
   }
 }
