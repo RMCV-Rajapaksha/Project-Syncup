21f0ca8e47b696a219b59d133deea40683af4b80
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-04-10 17:11:56 +0530

    basics added

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
 import 'package:project_syncup/pages/Create.dart';
+import 'package:project_syncup/pages/Login.dart';
+import 'package:project_syncup/pages/splash.dart';
 
 void main() {
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => const Create(),
+      builder: (context) => const Splash(),
     ),
   );
 }
