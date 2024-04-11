0837548138b0d7a91c216182366980920beb025d
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-04-11 13:13:34 +0530

    Update Navbar.dart with SpeedDial widget for menu options

---
+++ lib/commponent/Navbar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:project_syncup/commponent/theme.dart';
+
+class Navbar extends StatefulWidget {
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Template(
+      screenWidth: screenWidth,
+      screenHeight: screenHeight,
+      theChild: Scaffold(
+        backgroundColor: Colors.transparent,
+        floatingActionButton: SpeedDial(
+          animatedIcon: AnimatedIcons.menu_close,
+          animatedIconTheme: IconThemeData(size: 22.0),
+          backgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+          visible: true,
+          curve: Curves.bounceIn,
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.arrow_circle_down_outlined),
+              backgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+              onTap: () {
+                Navigator.pushNamed(context, '/create');
+              },
+              label: 'Change plan',
+              labelStyle: TextStyle(fontSize: 18.0),
+              labelBackgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.qr_code),
+              backgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+              onTap: () {
+                Navigator.pushNamed(context, '/chat');
+              },
+              label: 'Scan QR',
+              labelStyle: TextStyle(fontSize: 18.0),
+              labelBackgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.camera_alt),
+              backgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+              onTap: () {
+                Navigator.pushNamed(context, '/chat');
+              },
+              label: 'camera',
+              labelStyle: TextStyle(fontSize: 18.0),
+              labelBackgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.add),
+              backgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+              onTap: () {
+                Navigator.pushNamed(context, '/chat');
+              },
+              label: 'New event',
+              labelStyle: TextStyle(fontSize: 18.0),
+              labelBackgroundColor: const Color.fromRGBO(53, 131, 169, 1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
-import 'package:project_syncup/pages/Chat.dart';
-import 'package:project_syncup/pages/Create.dart';
+import 'package:project_syncup/commponent/Navbar.dart';
 
 void main() {
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => const ChatPage(),
+      builder: (context) => Navbar(),
     ),
   );
 }
