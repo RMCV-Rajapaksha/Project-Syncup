48f04672d0458762c06cd2bc2e30f02d2feb84da
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-04-14 10:33:45 +0530

    intropages content added

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,14 @@ import 'package:project_syncup/commponent/Navbar.dart';
 import 'package:project_syncup/pages/Login.dart';
 import 'package:project_syncup/pages/Onboarding.dart';
 import 'package:project_syncup/pages/intro_pages/page1.dart';
+import 'package:project_syncup/pages/intro_pages/page2.dart';
+import 'package:project_syncup/pages/intro_pages/page3.dart';
 
 void main() {
   runApp(
     DevicePreview(
       enabled: true,
-      builder: (context) => Intro_page1(),
+      builder: (context) => Intro_page3(),
     ),
   );
 }
---
+++ lib/pages/intro_pages/page2.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:project_syncup/commponent/theme.dart';
+
+class Intro_page2 extends StatelessWidget {
+  const Intro_page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Template(
+      screenWidth: screenWidth, 
+      screenHeight: screenHeight, 
+      theChild: 
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.1),
+              Image.asset('assets/logos/SyncUp_logo.png', width: screenWidth * 0.2,),
+              const Text("SyncUp",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 48,
+              ),),
+              const Text("How SyncUP Works",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                ),
+              textAlign: TextAlign.left,),
+              const Text("Join an Event:",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 22,
+                ),
+                textAlign: TextAlign.left,),
+                const Text("Enter the event code or scan the QR to join the event's live feed.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.left,),
+                const Text("Share Your Moment:",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 22,
+                ),
+                textAlign: TextAlign.left,),
+                const Text("Upload your photos and videos to contribute to the event's collective memory.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.left,),
+                const Text("Relive the Experience:",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 22,
+                ),
+                textAlign: TextAlign.left,),
+                const Text("Browse through a rich tapestry of shared experiences from multiple perspectives.",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.left,),
+            ],
+          ),
+        )
+      );
+  }
+}
