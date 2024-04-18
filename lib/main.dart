cee9c0ae0f2154bdfd33f6d3489e01e6f6e9001a
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-04-18 20:16:24 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:device_preview/device_preview.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:project_syncup/commponent/Navbar.dart';
 import 'package:project_syncup/pages/Chat.dart';
 import 'package:project_syncup/pages/Login.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(
+    Duration(seconds: 2)
+    );
+    FlutterNativeSplash.remove();
+
+
   runApp(
     DevicePreview(
       enabled: true,
